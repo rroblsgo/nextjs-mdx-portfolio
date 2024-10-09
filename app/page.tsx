212e2deb66e421adbc27a1c97feef7fc82bca4d6
Ricardo Robles <rroblesgo@gmail.com>
2024-10-09 21:58:14 +0200

    End Part - II

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Intro from '@/components/intro'
+import { MDXRemote } from 'next-mdx-remote/rsc'
 
 export default function Home() {
+  const content = `
+    # This is a markdown heading
+  `
   return (
     <section className='pb-24 pt-40'>
       <div className='container max-w-3xl'>
         <Intro />
+
+        <MDXRemote source={content} />
       </div>
     </section>
   )
---
+++ app/posts/page.tsx
@@ -0,0 +1,3 @@
+export default function Postspage() {
+  return <div>Posts Page</div>
+}
